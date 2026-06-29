41b834957a5789a5361f19e6e2825b72795ea5c1
daviddlaa <david.ec21@gmail.com>
2026-06-29 09:47:36 -0500

    borrado masivo de gestiones maestro en escritorio 001

--- commit_push.bat
+++ commit_push.bat
@@ -4,6 +4,6 @@ git add -A
 git add src/config/db.js
 git add src/config/initDb.pg.js
 git add src/controllers/gestionesMaestro.controller.js
-git commit -m "borrado masivo de gestiones maestro en escritorio"
+git commit -m "borrado masivo de gestiones maestro en escritorio 001"
 git push origin master
 pause
